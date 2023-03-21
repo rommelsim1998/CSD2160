7514ee95c787b07ceb296c51f2139808d0adfb7a
rommelsim1998 <zhenpengrommel.sim@digipen.edu>
2023-03-21 21:21:23 +0800

    alvin senpai
    help why keep reading 2?

--- NetworkingSystem/System.cpp
+++ NetworkingSystem/System.cpp
@@ -124,22 +124,18 @@ void Server::Send(void* buffer, int len)
 }
 void Server::Send(int x, int y)
 {
+	// Way to read!
 	std::memset(m_buffer, 0, MTU);
 	std::memcpy(m_buffer, &x, sizeof(x));
 	std::memcpy(m_buffer + sizeof(x), &y, sizeof(y));
-
-	float _x, _y;
-	std::memcpy(&_x, m_buffer, 4);
-	std::memcpy(&_y, m_buffer + 4, 4);
-	std::cout << "Checking: " << _x << ", " << _y << std::endl;			// result of _x and _y shold ne the same as x and y
-
+	int len = sizeof(float) * 2;
 	for (int i = 0; i < clientAddresses.size(); ++i)
 	{
 		if (clientAddresses[i].sin_addr.S_un.S_addr == 0)
 			continue;
 
-		sendto(m_recvSocket, reinterpret_cast<const char*>(m_buffer), MTU, 0, reinterpret_cast<SOCKADDR*>(&clientAddresses[i]), sizeof(clientAddresses[i]));
-		std::cout << "[Server]: Sending " << MTU << " bytes of data to " << clientAddresses[i].sin_addr.S_un.S_addr << std::endl;
+		sendto(m_recvSocket, reinterpret_cast<const char*>(m_buffer), len, 0, reinterpret_cast<SOCKADDR*>(&clientAddresses[i]), sizeof(clientAddresses[i]));
+		std::cout << "[Server]: Sending " << len << " bytes of data to " << clientAddresses[i].sin_addr.S_un.S_addr << std::endl;
 	}
 }
 
@@ -204,6 +200,7 @@ void Client::Update()
 
 void Client::Send(void* buffer, int len)
 {
+	std::memset(m_buffer, 0, MTU);
 	sendto(m_sendSocket, (const char*)buffer, len, 0, reinterpret_cast<SOCKADDR*>(&m_serverAddr), sizeof(m_serverAddr));
 	std::cout << "[Client]: Sending " << len << " bytes of data\n";
 
@@ -211,13 +208,16 @@ void Client::Send(void* buffer, int len)
 
 void Client::Read(int& value)
 {
+	std::memset(m_buffer, 0, MTU);
 	int bytes = recvfrom(m_sendSocket, m_buffer, MTU, 0, nullptr, nullptr);
 	value = *(int*)(m_buffer);
+	
 }
 
 
 void Client::Read(float& x, float& y)
 {
+	std::memset(m_buffer, 0, MTU);
 	int bytes = recvfrom(m_sendSocket, m_buffer, MTU, 0, nullptr, nullptr);
 	if (bytes == SOCKET_ERROR)
 	{
@@ -227,10 +227,13 @@ void Client::Read(float& x, float& y)
 	}
 	else
 	{
-		//std::cout << "[Client]: Receiving " << bytes << " of data. Message is: " << *(float*)(m_buffer) << std::endl;
-		std::memcpy((float*)&x, m_buffer, 4);
+		
+		std::cout << "[Client]: " << static_cast<float>(*m_buffer) << ", " << static_cast<float>(*(m_buffer + 4)) << std::endl;
+		x = static_cast<float>(*m_buffer);
+		y = static_cast<float>(*(m_buffer + 4));
+		/*std::memcpy((float*)&x, m_buffer, 4);
 		std::memcpy((float*)&y, m_buffer + 4, 4);
-		std::cout << "[Client]: Receiving " << bytes << " of data. Message is: " << x << ", " << y << std::endl;
+		std::cout << "[Client]: Receiving " << bytes << " of data. Message is: " << x << ", " << y << std::endl;*/
 	}
 	//std::cout << "[Client]: Bytes read: " << bytes << std::endl;
 	//std::memcpy(&x, m_buffer, 4);		// x 
